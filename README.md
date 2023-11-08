525561c09835b322504664773e5ebccf7fdee2ae
ehren strifling <ehrenstrifling@gmail.com>
2023-11-08 14:42:31 -0600

    Code done.

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Reading Controller inputs with the Gamepad API
 With javascript, you can develop games that can run in anyone's web browser. Keyboard and Mouse inputs work fine but have you ever wanted to make your games playable with a controller? To make your game play just like a console game? With the controller api, you can do that.
 
+## Events
+There are 2 events in the gamepad api: **'gamepadconnected'** and **'gamepaddisconnected'**.  
+The gamepadconnected event is called whenever a gamepad is connected or, when a button is first pressed if the controller was connected before the page loaded.  
+The gamepaddisconnected event is called whenever a gamepad is disconnected.
+Gamepad events will have a **'gamepad'** property which whill be a Gamepad object.
+
+## navigator.getGamepads
+The navigator.getGamepads method will return an array of all connected gamepads. This is
 ## The main loop
 Since there is no event for when a button is pressed on the gamepad, we will have to use a loop on a timer instead. Fortunately, this is already how most games run.  
 For this project's main loop, I will be using setTimout to run code 60 times every second.  
 ![const frameDelay = 1000/60; let running = true; async function main() { while (running) { /* ... */ await sleep(frameDelay); }}](src/img/main.png)
+
+## The Gamepad object
+The gamepad object has 4 non-array properties and 2 array properties.  
+index: Unique id given by the browser to this controller. No 2 controller will share the same index.
+
+![](src/img/controller.png)
--- src/scripts/gamepad-reader.js
+++ src/scripts/gamepad-reader.js
@@ -19,16 +19,122 @@ class GamepadReader {
 
     window.addEventListener('gamepadconnected', e=>this.onControllerConnected(e));
     window.addEventListener('gamepaddisconnected', e=>this.onControllerDisconnected(e));
-
-    // this.onControllerConnected({gamepad: {index: 0}});
-    // this.onControllerConnected({gamepad: {index: 1}});
-    // this.onControllerConnected({gamepad: {index: 2}});
-
-    //this.onControllerDisconnected({index: 1});
   }
 
   tick() {
+    if (this.activeGamepadIndex>=0) {
+      let gamepad = navigator.getGamepads().find(g=>{return g!==null && g.index===this.controllerBar[this.activeGamepadIndex].index;})
+      if (gamepad) {
+        
 
+        //update info
+        let infoP = this.element.querySelector(".controller-info > p");
+        //I don't have the time so I'm bringing out the <br> tag
+        infoP.innerHTML = `<b>Index</b>: ${gamepad.index}`;
+        infoP.innerHTML += `<br><b>Id</b>: ${gamepad.id}`;
+        infoP.innerHTML += `<br><b>Mapping</b>: ${gamepad.mapping}`;
+        infoP.innerHTML += `<br><b>Timestamp</b>: ${gamepad.timestamp}`;
+
+        // infoP.innerHTML += `<br><b>Hand</b>: `; //Firefox is trolling me hard with this one
+        // if (gamepad.hand) {
+        //   infoP.innerHTML += `${gamepad.hand}`;
+        // } else {
+        //   infoP.innerHTML += `unavailable in this browser`;
+        // }
+        //update axis
+        let axis = [...this.element.querySelectorAll(".controller-axis > .dual-axis")];
+        for (let i=0;i<gamepad.axes.length;++i) {
+          let a = Math.floor(i/2);
+          if (i&1) {
+            axis[a].querySelector(".circle").style.top = `${gamepad.axes[i] * 50 + 25}px`;
+          } else {
+            axis[a].querySelector(".circle").style.left = `${gamepad.axes[i] * 50 + 25}px`;
+          }
+        }
+        //update buttons
+        let buttons = [...this.element.querySelectorAll(".controller-buttons > .button")];
+        buttons.forEach((e,i) => {
+          let pressed = e.querySelector(".pressed");
+          if (gamepad.buttons[i].pressed) {
+            pressed.classList.add("active");
+          } else {
+            pressed.classList.remove("active");
+          }
+          let analog = e.querySelector(".analog > .analog-fill");
+          analog.style.width = `${gamepad.buttons[i].value * 100}%`;
+        });
+      }
+    }
+  }
+
+  selectionChanged() {
+    let container = this.element.querySelector(".controller-container");
+    if (container) {
+      container.remove();
+    }
+    if (this.activeGamepadIndex>=0) {
+      /** @type {Gamepad} */
+      let gamepad = navigator.getGamepads().find(g=>{return g!==null && g.index===this.controllerBar[this.activeGamepadIndex].index;});
+      container = document.createElement("div");
+      container.classList.add("controller-container");
+      this.element.appendChild(container);
+      
+      let left = document.createElement("div");
+      let right = document.createElement("div");
+      left.classList.add("col-2");
+      right.classList.add("col-2");
+
+      container.appendChild(left);
+      container.appendChild(right);
+
+      //create info section
+      let info = document.createElement("div");
+      info.classList.add("controller-info")
+      let infoH2 = document.createElement("h2");
+      infoH2.innerHTML = "Controller info";
+      info.appendChild(infoH2);
+      let infoP = document.createElement("p");
+      info.appendChild(infoP);
+
+      left.appendChild(info);
+
+      //create axis section
+      let axis = document.createElement("div");
+      axis.classList.add("controller-axis");
+      for (let i=0;i<gamepad.axes.length;i+=2) {
+        let dualAxis = document.createElement("div");
+        dualAxis.classList.add("dual-axis");
+        let circle = document.createElement("div");
+        circle.classList.add("circle");
+        dualAxis.appendChild(circle);
+        axis.appendChild(dualAxis);
+      }
+      left.appendChild(axis);
+
+      //create buttons section
+      let buttons = document.createElement("div");
+      buttons.classList.add("controller-buttons");
+
+      for (let i=0;i<gamepad.buttons.length;++i) {
+        let button = document.createElement("div");
+        button.classList.add("button");
+        let p = document.createElement("p");
+        p.innerHTML = `button ${i}`;
+        button.appendChild(p);
+        let pressed = document.createElement("div");
+        pressed.classList.add("pressed");
+        button.appendChild(pressed);
+        let analog = document.createElement("div");
+        analog.classList.add("analog")
+        button.appendChild(analog);
+        let analogFill = document.createElement("div");
+        analogFill.classList.add("analog-fill");
+        analog.appendChild(analogFill);
+        buttons.appendChild(button);
+      }
+
+      right.appendChild(buttons);
+    }
   }
 
   addController(gamepad) {
@@ -42,20 +148,22 @@ class GamepadReader {
       }
       this.activeGamepadIndex = this.controllerBar.indexOf(controllerReader);
       this.setIndex(this.activeGamepadIndex);
+      this.selectionChanged();
     });
   }
   removeController(index) {
-
+    this.controllerBar[index].element.remove();
     if (this.activeGamepadIndex>=index) {
       this.unsetIndex(this.activeGamepadIndex);
-      this.controllerBar[index].element.remove();
       this.controllerBar.splice(index,1);
       if (this.activeGamepadIndex===index) {
         this.activeGamepadIndex = -1;
+        this.selectionChanged();
       } else if (this.activeGamepadIndex>index) {
         this.activeGamepadIndex--;
         this.setIndex(this.activeGamepadIndex);
       }
+      this.selectionChanged();
     } else {
       this.controllerBar.splice(index,1);
     } 
@@ -69,6 +177,7 @@ class GamepadReader {
   }
 
   onControllerConnected(e) {
+    document.getElementById("no-controller").style.display = "none";
     this.addController(e.gamepad);
 
     if (this.activeGamepadIndex>=0) {
@@ -76,6 +185,7 @@ class GamepadReader {
     }
     this.activeGamepadIndex = this.controllerBar.length-1;
     this.setIndex(this.activeGamepadIndex);
+    this.selectionChanged();
   }
   onControllerDisconnected(e) {
     this.controllerBar.forEach((c, i)=>{
@@ -83,13 +193,15 @@ class GamepadReader {
         this.removeController(i);
       }
     });
+    if (this.controllerBar.length===0) {
+      document.getElementById("no-controller").style.display = "inline";
+    }
   }
 } 
 
 
 const frameDelay = 1000/60;
 let running = true;
-
 async function main() {
   let gamepadReaders = [];
   [...document.getElementsByClassName("gamepad-reader")].forEach(element=>{
